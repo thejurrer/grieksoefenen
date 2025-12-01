87cb75147fa0da1e186bd253c1a4c18f5bed5060
TheJurrer <snippejurre@gmail.com>
2025-12-01 09:30:24 +0100

    fix for doing text 1314

--- grieks oefenen/grieksoefen.py
+++ grieks oefenen/grieksoefen.py
@@ -101,6 +101,12 @@ if __name__ == "__main__":
         elif tekstchoice == "12":
             tekst = d.griekseWoordentekst12
             grieksNederlands(tekst)
+        elif tekstchoice == "13":
+            tekst = d.griekseWoordentekst13
+            grieksNederlands(tekst)
+        elif tekstchoice == "14":
+            tekst = d.griekseWoordentekst14
+            grieksNederlands(tekst)
         elif tekstchoice == "alles":
             alles = d.griekseWoordentekst2 | d.griekseWoordentekst3 | d.griekseWoordentekst4 | d.griekseWoordentekst6 | d.griekseWoordentekst7 | d.griekseWoordentekst8 | d.griekseWoordentekst9 | d.griekseWoordentekst11
             grieksNederlands(alles)
